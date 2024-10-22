13d2498405289d230dcaace9924e10ff67003448
Vova <tehnicalmailone@gmail.com>
2024-10-22 20:35:31 +1000

    test

--- src/Ryujinx/Program.cs
+++ src/Ryujinx/Program.cs
@@ -31,6 +31,7 @@ namespace Ryujinx.Ava
 {
     internal partial class Program
     {
+        //
         public static double WindowScaleFactor { get; set; }
         public static double DesktopScaleFactor { get; set; } = 1.0;
         public static string Version { get; private set; }
