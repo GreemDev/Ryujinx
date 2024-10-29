c52c97869b3d6eff231937005ae05d2d2acd1fed
KeatonTheBot <onikeaton@gmail.com>
2024-10-29 16:12:24 -0500

    Revert "Vulkan: Feedback loop detection and barriers (#7226)"
    This reverts commit ca59c3f4998e2d1beb3b0d0214611e3332238557.

--- src/Ryujinx.Headless.SDL2/Program.cs
+++ src/Ryujinx.Headless.SDL2/Program.cs
@@ -7,7 +7,6 @@ using Ryujinx.Common.Configuration.Hid;
 using Ryujinx.Common.Configuration.Hid.Controller;
 using Ryujinx.Common.Configuration.Hid.Controller.Motion;
 using Ryujinx.Common.Configuration.Hid.Keyboard;
-using Ryujinx.Common.GraphicsDriver;
 using Ryujinx.Common.Logging;
 using Ryujinx.Common.Logging.Targets;
 using Ryujinx.Common.SystemInterop;
@@ -460,8 +459,6 @@ namespace Ryujinx.Headless.SDL2
             GraphicsConfig.MaxAnisotropy = option.MaxAnisotropy;
             GraphicsConfig.ShadersDumpPath = option.GraphicsShadersDumpPath;
             GraphicsConfig.EnableMacroHLE = !option.DisableMacroHLE;
-
-            DriverUtilities.InitDriverConfig(option.BackendThreading == BackendThreading.Off);
 
             while (true)
             {
