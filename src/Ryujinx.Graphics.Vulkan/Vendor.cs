7ead9a7918115172827159008497b6165d8118ef
KeatonTheBot <onikeaton@gmail.com>
2024-11-05 20:29:58 -0600

    nuget: bump packages
    * Bump Silk.NET.Vulkan group with 3 updates (2.21.0 to 2.22.0)
    
    * Bump Microsoft.IdentityModel.JsonWebTokens from 8.1.2 to 8.2.0

--- src/Ryujinx.Graphics.Vulkan/Vendor.cs
+++ src/Ryujinx.Graphics.Vulkan/Vendor.cs
@@ -92,7 +92,7 @@ namespace Ryujinx.Graphics.Vulkan
                 DriverId.MesaDozen => "Dozen",
                 DriverId.MesaNvk => "NVK",
                 DriverId.ImaginationOpenSourceMesa => "Imagination (Open)",
-                DriverId.MesaAgxv => "Honeykrisp",
+                DriverId.MesaHoneykrisp => "Honeykrisp",
                 _ => id.ToString(),
             };
         }
