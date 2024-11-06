4831965404f3b84de65a43ff28985cddf1423936
TheToid <amurgshere@gmail.com>
2024-11-07 09:37:30 +1000

    Add ability to trim and untrim XCI files from the application context menu AND in Bulk (#105)

---
+++ src/Ryujinx/UI/Helpers/XCITrimmerFileStatusConverter.cs
@@ -0,0 +1,46 @@
+using Avalonia;
+using Avalonia.Data;
+using Avalonia.Data.Converters;
+using Ryujinx.Ava.Common.Locale;
+using Ryujinx.UI.Common.Models;
+using System;
+using System.Globalization;
+using static Ryujinx.Common.Utilities.XCIFileTrimmer;
+
+namespace Ryujinx.Ava.UI.Helpers
+{
+    internal class XCITrimmerFileStatusConverter : IValueConverter
+    {
+        public static XCITrimmerFileStatusConverter Instance = new();
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is UnsetValueType)
+            {
+                return BindingOperations.DoNothing;
+            }
+
+            if (!targetType.IsAssignableFrom(typeof(string)))
+            {
+                return null;
+            }
+
+            if (value is not XCITrimmerFileModel app)
+            {
+                return null;
+            }
+
+            return app.PercentageProgress != null ? String.Empty :
+                app.ProcessingOutcome != OperationOutcome.Successful && app.ProcessingOutcome != OperationOutcome.Undetermined ? LocaleManager.Instance[LocaleKeys.TitleXCIStatusFailedLabel] :
+                app.Trimmable & app.Untrimmable ? LocaleManager.Instance[LocaleKeys.TitleXCIStatusPartialLabel] :
+                app.Trimmable ? LocaleManager.Instance[LocaleKeys.TitleXCIStatusTrimmableLabel] :
+                app.Untrimmable ? LocaleManager.Instance[LocaleKeys.TitleXCIStatusUntrimmableLabel] :
+                String.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
