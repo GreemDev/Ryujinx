0a7f7d1ecdbe4fecad06cd7d9b6c473424b69cb2
Barış Hamil <barishamil90@gmail.com>
2024-11-17 21:47:53 +0300

    CycleControllers: Localized "Player X", fixed button vertical padding

--- src/Ryujinx/UI/ViewModels/CycleController.cs
+++ src/Ryujinx/UI/ViewModels/CycleController.cs
@@ -1,3 +1,4 @@
+using Ryujinx.Ava.Common.Locale;
 using Ryujinx.Common.Configuration.Hid;
 
 namespace Ryujinx.Ava.UI.ViewModels
@@ -29,7 +30,18 @@ namespace Ryujinx.Ava.UI.ViewModels
 
         public CycleController(int v, Key x)
         {
-            Player = $"Player {v}";
+            Player = v switch
+            {
+                1 => LocaleManager.Instance[LocaleKeys.ControllerSettingsPlayer1],
+                2 => LocaleManager.Instance[LocaleKeys.ControllerSettingsPlayer2],
+                3 => LocaleManager.Instance[LocaleKeys.ControllerSettingsPlayer3],
+                4 => LocaleManager.Instance[LocaleKeys.ControllerSettingsPlayer4],
+                5 => LocaleManager.Instance[LocaleKeys.ControllerSettingsPlayer5],
+                6 => LocaleManager.Instance[LocaleKeys.ControllerSettingsPlayer6],
+                7 => LocaleManager.Instance[LocaleKeys.ControllerSettingsPlayer7],
+                8 => LocaleManager.Instance[LocaleKeys.ControllerSettingsPlayer8],
+                _ => LocaleManager.Instance[LocaleKeys.ControllerSettingsPlayer] + " " + v
+            };
             Hotkey = x;
         }
     }
