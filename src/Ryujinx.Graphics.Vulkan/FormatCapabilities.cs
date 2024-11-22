b58e353a55b21f06dd078b1b05361bdfc186e940
sunshineinabox <aqemail@gmail.com>
2024-11-21 18:45:11 -0800

    Resolve Image Usage Validation Error

--- src/Ryujinx.Graphics.Vulkan/FormatCapabilities.cs
+++ src/Ryujinx.Graphics.Vulkan/FormatCapabilities.cs
@@ -148,7 +148,7 @@ namespace Ryujinx.Graphics.Vulkan
             return (formatFeatureFlags & flags) == flags;
         }
 
-        public VkFormat ConvertToVkFormat(Format srcFormat)
+        public VkFormat ConvertToVkFormat(Format srcFormat, bool storageFeatureFlagRequired)
         {
             var format = FormatTable.GetFormat(srcFormat);
 
@@ -165,7 +165,7 @@ namespace Ryujinx.Graphics.Vulkan
                 requiredFeatures |= FormatFeatureFlags.ColorAttachmentBit;
             }
 
-            if (srcFormat.IsImageCompatible())
+            if (srcFormat.IsImageCompatible() && storageFeatureFlagRequired)
             {
                 requiredFeatures |= FormatFeatureFlags.StorageImageBit;
             }
