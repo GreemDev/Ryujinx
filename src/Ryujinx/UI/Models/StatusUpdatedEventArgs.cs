2e6794e69b33405990aff15e7dbe5a0f589666bf
Keaton <onikeaton@gmail.com>
2024-11-25 13:39:09 -0600

    Add custom refresh rate mode to VSync option (#238)
    Rebased @jcm93's refreshinterval branch:
    https://github.com/jcm93/Ryujinx/tree/refreshinterval
    
    The option is placed under System/Hacks. Disabled, it's the default
    Ryujinx behavior. Enabled, the behavior is shown in the attached
    screenshots. If a framerate is too high or low, you can adjust the value
    where you normally toggle VSync on and off. It will also cycle through
    the default on/off toggles.
    
    Also, in order to reduce clutter, I made an adjustment to remove the
    target FPS and only show the percentage.
    
    ---------
    
    Co-authored-by: jcm <6864788+jcm93@users.noreply.github.com>

--- src/Ryujinx/UI/Models/StatusUpdatedEventArgs.cs
+++ src/Ryujinx/UI/Models/StatusUpdatedEventArgs.cs
@@ -4,18 +4,17 @@ namespace Ryujinx.Ava.UI.Models
 {
     internal class StatusUpdatedEventArgs : EventArgs
     {
-        public bool VSyncEnabled { get; }
+        public string VSyncMode { get; }
         public string VolumeStatus { get; }
         public string AspectRatio { get; }
         public string DockedMode { get; }
         public string FifoStatus { get; }
         public string GameStatus { get; }
-
         public uint ShaderCount { get; }
 
-        public StatusUpdatedEventArgs(bool vSyncEnabled, string volumeStatus, string dockedMode, string aspectRatio, string gameStatus, string fifoStatus, uint shaderCount)
+        public StatusUpdatedEventArgs(string vSyncMode, string volumeStatus, string dockedMode, string aspectRatio, string gameStatus, string fifoStatus, uint shaderCount)
         {
-            VSyncEnabled = vSyncEnabled;
+            VSyncMode = vSyncMode;
             VolumeStatus = volumeStatus;
             DockedMode = dockedMode;
             AspectRatio = aspectRatio;
