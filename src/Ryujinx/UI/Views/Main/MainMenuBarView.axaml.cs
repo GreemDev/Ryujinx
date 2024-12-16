1a7b3bf874e1a6aa04ee43d83d7baddabaffc3c6
LotP1 <rasmus.stilling.pedersen1@gmail.com>
2024-12-16 21:55:46 +0100

    cleanup

--- src/Ryujinx/UI/Views/Main/MainMenuBarView.axaml.cs
+++ src/Ryujinx/UI/Views/Main/MainMenuBarView.axaml.cs
@@ -19,8 +19,6 @@ using Ryujinx.UI.Common.Helper;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text.Json;
-using System.Text.Json.Serialization.Metadata;
 
 namespace Ryujinx.Ava.UI.Views.Main
 {
