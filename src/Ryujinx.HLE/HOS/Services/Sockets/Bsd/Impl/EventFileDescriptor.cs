4171913bafa782913f059cd3a62becc2e6194bb2
Evan Husted <gr33m11@gmail.com>
2024-12-21 17:05:46 -0600

    misc: One additional usage of Lock & comment why it's not used on the others.

--- src/Ryujinx.HLE/HOS/Services/Sockets/Bsd/Impl/EventFileDescriptor.cs
+++ src/Ryujinx.HLE/HOS/Services/Sockets/Bsd/Impl/EventFileDescriptor.cs
@@ -10,6 +10,7 @@ namespace Ryujinx.HLE.HOS.Services.Sockets.Bsd.Impl
         private ulong _value;
         private readonly EventFdFlags _flags;
 
+        // type is not Lock due to Monitor class usage
         private readonly object _lock = new();
 
         public bool Blocking { get => !_flags.HasFlag(EventFdFlags.NonBlocking); set => throw new NotSupportedException(); }
