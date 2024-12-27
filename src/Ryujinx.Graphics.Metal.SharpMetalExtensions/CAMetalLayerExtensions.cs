9558b3771634d144501dedf3f081afc485a49d9d
Evan Husted <greem@greemdev.net>
2024-12-26 22:42:39 -0600

    metal: Try and see if this lets VSync turn off.

---
+++ src/Ryujinx.Graphics.Metal.SharpMetalExtensions/CAMetalLayerExtensions.cs
@@ -0,0 +1,21 @@
+﻿using SharpMetal;
+using SharpMetal.ObjectiveCCore;
+using SharpMetal.QuartzCore;
+using System.Runtime.Versioning;
+// ReSharper disable InconsistentNaming
+
+namespace Ryujinx.Graphics.Metal.SharpMetalExtensions
+{
+    [SupportedOSPlatform("OSX")]
+    public static class CAMetalLayerExtensions
+    {
+        private static readonly Selector sel_displaySyncEnabled = "displaySyncEnabled";
+        private static readonly Selector sel_setDisplaySyncEnabled = "setDisplaySyncEnabled:";
+        
+        public static bool IsDisplaySyncEnabled(this CAMetalLayer metalLayer) 
+            => ObjectiveCRuntime.bool_objc_msgSend(metalLayer.NativePtr, sel_displaySyncEnabled);
+
+        public static void SetDisplaySyncEnabled(this CAMetalLayer metalLayer, bool enabled) 
+            => ObjectiveCRuntime.objc_msgSend(metalLayer.NativePtr, sel_setDisplaySyncEnabled, enabled);
+    }
+}
